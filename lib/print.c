89bff6c60dd2948699e2a5e20ba167fbdb17b662
赵开明 <24371102@buaa.edu.cn>
2026-03-25 18:25:21 +0800

    correct again

--- lib/print.c
+++ lib/print.c
@@ -32,13 +32,13 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 
 		/* check "are we hitting the end?" */
 		/* Exercise 1.4: Your code here. (3/8) */
-		if(*temp == '\0') {
+		if(*fmt == '\0') {
 			break;
 		}
 
 		/* we found a '%' */
 		/* Exercise 1.4: Your code here. (4/8) */
-		if(*temp == '%') {
+		if(*fmt == '%') {
 			fmt++;
 		}
 		
